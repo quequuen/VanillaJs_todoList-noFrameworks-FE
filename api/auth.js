3b2a3708d546b981c1809f5ae71fc3ebd4ecdc7f
quequuen <rkdusl2020@gmail.com>
2025-11-08 16:09:40 +0900

    feat: 인증 및 Todo API 스켈레톤 추가

--- api/auth.js
+++ api/auth.js
@@ -1,1 +1,37 @@
 // 인증 관련 api
+import api from "./api";
+
+const AUTH_ENDPOINTS = {
+  sendMagicLink: "", // TODO: 엔드포인트 설정
+  verifyMagicLink: "", // TODO: 엔드포인트 설정
+  logout: "", // TODO: 엔드포인트 설정
+};
+
+const resolveEndpoint = (key) => {
+  const endpoint = AUTH_ENDPOINTS[key];
+  if (!endpoint) {
+    throw new Error(`AUTH_ENDPOINTS.${key}가 아직 설정되지 않았습니다.`);
+  }
+  return endpoint;
+};
+
+export const sendMagicLink = async (email) => {
+  // TODO: payload 구조 확정 후 구현 세부화
+  return api.post(resolveEndpoint("sendMagicLink"), { email });
+};
+
+export const verifyMagicLink = async (token) => {
+  // TODO: 토큰 검증 로직 구현 세부화
+  return api.get(resolveEndpoint("verifyMagicLink"), { params: { token } });
+};
+
+export const logout = async () => {
+  // TODO: 로그아웃 로직 구현 세부화
+  return api.post(resolveEndpoint("logout"));
+};
+
+export default {
+  sendMagicLink,
+  verifyMagicLink,
+  logout,
+};
--- api/todos.js
+++ api/todos.js
@@ -1,1 +1,52 @@
 //  todo 관련 api
+import api from "./api";
+
+const TODO_ENDPOINTS = {
+  list: "", // TODO: 엔드포인트 설정
+  detail: "", // TODO: 엔드포인트 설정 (예: `/api/todos/{id}`)
+  create: "", // TODO: 엔드포인트 설정
+  update: "", // TODO: 엔드포인트 설정 (예: `/api/todos/{id}`)
+  remove: "", // TODO: 엔드포인트 설정 (예: `/api/todos/{id}`)
+  toggle: "", // TODO: 엔드포인트 설정 (예: `/api/todos/{id}/toggle`)
+};
+
+const resolveEndpoint = (key) => {
+  const endpoint = TODO_ENDPOINTS[key];
+  if (!endpoint) {
+    throw new Error(`TODO_ENDPOINTS.${key}가 아직 설정되지 않았습니다.`);
+  }
+  return endpoint;
+};
+
+export const getTodos = async (params = {}) => {
+  return api.get(resolveEndpoint("list"), { params });
+};
+
+export const getTodoById = async (id) => {
+  return api.get(resolveEndpoint("detail"), { params: { id } });
+};
+
+export const createTodo = async (payload) => {
+  return api.post(resolveEndpoint("create"), payload);
+};
+
+export const updateTodo = async (id, payload) => {
+  return api.put(resolveEndpoint("update"), { id, ...payload });
+};
+
+export const deleteTodo = async (id) => {
+  return api.delete(resolveEndpoint("remove"), { data: { id } });
+};
+
+export const toggleTodo = async (id) => {
+  return api.patch(resolveEndpoint("toggle"), { id });
+};
+
+export default {
+  getTodos,
+  getTodoById,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+  toggleTodo,
+};
